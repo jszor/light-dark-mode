b7b523764a808983627efc15cca6168077173711
Juan Salvador Zorrilla <jsalvadorzorrilla@gmail.com>
2024-06-19 14:13:00 +0200

    saving light or dark mode preference in local storage

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function imageMode(color) {
     image4.src = `img/undraw_campfire_${color}.svg`;
 }
 
-
 // Dark Mode Styles
 
 function darkMode() {
@@ -40,12 +39,26 @@ function lightMode() {
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
     }
 }
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage for Theme
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
